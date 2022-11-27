1c164427bf9141eca8c6be5a5e7bae1f4e1b604e
Matthew Eastman <matthew.m.eastman@gmail.com>
2022-11-26 22:36:29 -0800

    New code files added

---
+++ escape/common.py
@@ -0,0 +1,23 @@
+# All variables are set to their starting values
+
+# Map Room variables
+current_room = 31
+top_left_x = 100
+top_left_y = 150
+wall_transparency_frame = 0
+
+# Player variables
+player_y = 2
+player_x = 5
+player_offset_x = 0
+player_offset_y = 0
+player_direction = "down"
+player_frame = 0
+air = 100
+energy = 100
+suit_stitched = False
+air_fixed = False
+items_player_may_carry = list(range(53, 82))
+items_player_may_stand_on = items_player_may_carry + [0, 2, 39, 48]
+
+launch_frame = 0
